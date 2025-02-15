76f218e747b3b103a58a681f26ad599ae4b101e4
ZaneMODell <odellzan@msu.edu>
2025-02-14 21:34:26 -0500

    changed string

--- app/src/main/java/com/example/fossdocs/MainActivity.kt
+++ app/src/main/java/com/example/fossdocs/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             FOSSDocsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "FOSSDocs",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     FOSSDocsTheme {
-        Greeting("Android")
+        Greeting("FOSSDocs")
     }
 }
