19f18735fe352e102282eabb80c48cb3bb16c412
ZaneMODell <94186155+ZaneMODell@users.noreply.github.com>
2025-02-21 11:23:12 -0500

    update to coil 3

--- app/src/main/kotlin/com/example/fossdocs/screencomponents/DocumentPreviewCard.kt
+++ app/src/main/kotlin/com/example/fossdocs/screencomponents/DocumentPreviewCard.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
+import coil3.compose.rememberAsyncImagePainter
 import com.example.fossdocs.R
 import com.example.fossdocs.viewmodels.DocumentPreviewVM
 
